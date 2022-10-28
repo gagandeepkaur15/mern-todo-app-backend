7164d9eac14d8ead1a5b1898e73c744d8f8ccdf9
Gagandeep <gagandeepkaur1523@gmail.com>
2022-10-29 01:38:54 +0530

    Added endpoints

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
+const express = require('express');
+const Todo = require("./models/TodoModel");
+
+const app=express();
+const port = 3000
+app.use(express.json());
 
 main().catch(err => console.log(err));
 
 async function main() {
   await mongoose.connect('mongodb://localhost:27017/test');
-
 }
 
 var db = mongoose.connection;
@@ -14,10 +19,21 @@ db.once('open',function(){
     console.log("Connected!!")
 });
 
-const todoSchema = new mongoose.Schema({
-    title: String,
-    desc: String,
-});
+app.get('/fetchTodo', async (req, res) => {
+    const todos = await Todo.find;
+    res.json(todos);
+})
 
-const Todo = mongoose.model('Todo',todoSchema);
-
+app.post('/addTodo', (req, res) => {
+    const newTodo = new Todo({ todo: req.body.todo }); 
+    newTodo.save().then(()=>{
+        res.json(newTodo);
+        // res.send("Items saved to database")
+    }).catch(()=>{
+        res.status(400).send("Items not saved")
+    });
+})
+  
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
---
+++ models/TodoModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const todoSchema = new mongoose.Schema({
+    todo: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    date:{
+        type:Date,
+        default: Date.now
+    }
+});
+
+const Todo = mongoose.model('Todo',todoSchema);
+module.exports = Todo;
